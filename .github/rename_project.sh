4f28407d8083fd6def8d948d14374f38dcec0b3b
jkrgr0 <jkrgr0@users.noreply.github.com>
2024-09-20 08:01:55 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="jkrgr0"
+original_name="python_myspeed"
+original_urlname="python-myspeed"
+original_description="Awesome python_myspeed created by jkrgr0"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv python_myspeed $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
